57fbad649331aa757fdbe272e7f2fdb3d9a53ae1
realeve <realeve@qq.com>
2017-07-26 00:13:27 +0800

    隐藏曲线

--- src/pages/simplePage.vue
+++ src/pages/simplePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="{slide:!isSection,section:isSection,split:fill,about:isSection}">
-        <arc-line v-if="fill" :fill="fill"></arc-line>
+        <!-- <arc-line v-if="fill" :fill="fill"></arc-line> -->
         <div class="wrap">
             <h4>{{title}}</h4>
             <div>
