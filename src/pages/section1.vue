50f0811596ed9da14fb5b5f05db7ea7c25dcc19f
realeve <realeve@qq.com>
2017-07-26 12:18:10 +0800

    增加视频、图片、链接、引用样式

--- src/pages/section1.vue
+++ src/pages/section1.vue
@@ -15,28 +15,56 @@
                         <strong>行业外</strong>:本文研究数据集中在2017年4月1日至6月30日之间，部分商铺采用了历史数据。</p>
                 </div>
             </div>
-    
         </div>
-        <simple-page :title="'数据范围'" :content="content1"></simple-page>
+        <div class="slide">
+            <video loop muted autoplay data-autoplay>
+                <source src="/static/video/web_crawler_sample.mp4" type="video/mp4">
+            </video>
+            <div class="wrap">                
+                <div class="main_title">
+                    <h4>这里可以写一些文字</h4>
+                </div>
+            </div>
+        </div>
+        <div class="slide">
+            <div class="wrap">
+                <h4>数据周期</h4>
+                <div class="content">
+                    <blockquote>
+                        这里是一些文字引用示例本文研究数据集中在2017年4月1日至6月30日之间，部分商铺采用了历史数据。本文研究数据集中在2017年4月1日至6月30日之间，部分商铺采用了历史数据。
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+        <div class="slide">
+            <div class="wrap">
+                <h4>a标签及列表示例</h4>
+                <div class="content">
+                    <ul>
+                        <li>
+                            <a href="http://www.chinagoldcoin.net" target="_blank">中国金币</a>
+                        </li>
+                        <li>这里是列表示例</li>
+                        <li>这里是列表示例</li>
+                        <li>这里是列表示例</li>
+                        <li>这里是列表示例</li>
+                        <li>这里是列表示例</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="slide">
+            <div class="wrap">
+                <h4>图片示例</h4>
+                <div class="content">
+                    <img src="/static/img/main.png" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import SimplePage from './simplePage';
 export default {
-    components: {
-        SimplePage
-    },
-    data() {
-        return {
-            content1: `<p><strong>行业内</strong>:本文研究数据集中在2017年4月1日至6月30日之间，部分商铺采用了历史数据。</p>
-            <p><strong>行业外</strong>:本文研究数据集中在2017年4月1日至6月30日之间，部分商铺采用了历史数据。</p>`,
-        }
-    },
-    computed: {
-        typeStatus() {
-            return this.$store.state.typeStatus;
-        }
-    },
 };
 </script>
