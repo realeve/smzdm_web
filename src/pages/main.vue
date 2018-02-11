83b551d0f627a13236f73879397cab4ad038cc30
realeve <realeve@qq.com>
2018-02-11 19:16:40 +0800

    标题样式更新

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="section page">
-        <video v-if="mobile" class="up_video" loop muted autoplay data-autoplay>
-            <source src="/static/video/theme.mp4" type="video/mp4">
-        </video>
-        <div class="wrap">
-            <p class="tip">按
-                <span class="key">F11</span> 键进入全屏</p>
-            <div class="title">
-                <div class="main_title">
-                    <h2>什么值得卖</h2>
-                    <h3>中国贵金属网上销售市场分析</h3>
-                    <small>技术质量部 李宾</small>
-                    <small>企管规划部 倪震</small>
-                    <p v-if="!showFooter">www.cbpc.ltd</p>
-                </div>
-                <div class="footer" v-if="showFooter">
-                    <small>
-                        <a href="http://www.miitbeian.gov.cn/" rel="noreferrer" target="_blank">蜀ICP备17016569号</a>
-                    </small>
-                </div>
-            </div>
+  <div class="section page">
+    <video v-if="mobile" class="up_video" loop muted autoplay data-autoplay>
+      <source src="/static/video/theme.mp4" type="video/mp4">
+    </video>
+    <div class="wrap">
+      <p class="tip">按
+        <span class="key">F11</span> 键进入全屏</p>
+      <div class="title">
+        <div class="main_title">
+          <h2>什么值得卖</h2>
+          <h3>中国贵金属网上销售市场分析</h3>
+          <small>技术质量部 李宾</small>
+          <small>企管规划部 倪震</small>
+          <p v-if="!showFooter">www.cbpc.ltd</p>
         </div>
+        <div class="footer" v-if="showFooter">
+          <small>
+            <a href="http://www.miitbeian.gov.cn/" rel="noreferrer" target="_blank">蜀ICP备17016569号</a>
+          </small>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -70,6 +70,16 @@ export default {
   }
   h2 {
     font-size: 40pt;
+    background-image: -webkit-gradient(
+      linear,
+      0 0,
+      right 0,
+      from(#008aff),
+      to(#40e4d5)
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-weight: bold;
   }
   h3 {
     font-size: 30pt;
